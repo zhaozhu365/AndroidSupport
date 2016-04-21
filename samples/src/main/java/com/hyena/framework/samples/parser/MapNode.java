df67fd8b96416597d9145e04ca3b3bdbe7a05bff
yangzc106 <zhc_yang@163.com>
2016-04-21 22:51:03 +0800

    add:Map xml parse

--- samples/src/main/java/com/hyena/framework/samples/parser/MapNode.java
+++
@@ -1,9 +0,0 @@
-package com.hyena.framework.samples.parser;
-
-/**
- * Created by yangzc on 16/4/21.
- */
-public class MapNode {
-
-
-}
--- samples/src/main/java/com/hyena/framework/samples/parser/line/MapLine.java
+++ samples/src/main/java/com/hyena/framework/samples/parser/line/MapLine.java
@@ -1,12 +1,16 @@
 package com.hyena.framework.samples.parser.line;
 
-import com.hyena.framework.samples.parser.MapNode;
+
+import com.hyena.framework.samples.parser.node.MapNode;
 
 /**
  * Created by yangzc on 16/4/21.
  */
-public class MapLine {
+public class MapLine extends MapNode {
+
     public String mStyle;
-    public MapNode mFromNode;
-    public MapNode mToNode;
+
+    public MapLine(String id, int width, int height) {
+        super(id, width, height);
+    }
 }
