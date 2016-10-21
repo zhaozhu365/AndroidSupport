902b857d9c43ef1e725d1450de14dc9a5a57afcf
yangzc <yangzc@knowbox.cn>
2016-10-21 14:57:03 +0800

    bugfix

--- samples/src/main/java/com/hyena/framework/samples/MainActivity.java
+++ samples/src/main/java/com/hyena/framework/samples/MainActivity.java
@@ -1,9 +1,13 @@
 package com.hyena.framework.samples;
 
+import android.annotation.TargetApi;
+import android.app.AppOpsManager;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.view.WindowManager;
+import android.os.Process;
 
 import com.hyena.framework.app.activity.NavigateActivity;
 import com.hyena.framework.app.fragment.BaseFragment;
@@ -11,14 +15,9 @@ import com.hyena.framework.app.fragment.BaseUIFragment;
 import com.hyena.framework.app.fragment.BaseUIFragmentHelper;
 import com.hyena.framework.app.fragment.UIViewFactory;
 import com.hyena.framework.clientlog.LogUtil;
-import com.hyena.framework.network.HttpProvider;
 import com.hyena.framework.samples.evn.ViewFactoryImpl;
 import com.hyena.framework.samples.plugin.InstrumentationHook;
-import com.hyena.framework.samples.webview.WebViewBrowser;
-import com.hyena.framework.samples.webview.WebViewFragment;
-import com.hyena.framework.utils.HttpHelper;
-
-import java.io.File;
+import com.hyena.framework.utils.ToastUtils;
 
 public class MainActivity extends NavigateActivity {
 
@@ -37,6 +36,26 @@ public class MainActivity extends NavigateActivity {
 //            }
 //        }).start();
         showFragment(BaseUIFragment.newFragment(this, Pull2RefreshFragment.class, null));
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AppOpsManager appOpsManager = (AppOpsManager) getSystemService(Context.APP_OPS_SERVICE);
+        int checkOp = appOpsManager.checkOp(AppOpsManager.OPSTR_CAMERA, Process.myUid(), getPackageName());
+        if (checkOp == AppOpsManager.MODE_IGNORED) {
+            // 权限被拒绝了
+            ToastUtils.showShortToast(this, "ignored");
+        } else {
+            ToastUtils.showShortToast(this, "yes");
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        LogUtil.v("yangzc", "onActivityResult");
     }
 
     @Override
